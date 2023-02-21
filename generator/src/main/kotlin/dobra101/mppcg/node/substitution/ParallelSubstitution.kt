9435cbf0d22c105611c21e7f3563bbf449b3d718
Dominik Brandt <brandtdominik@t-online.de>
2023-02-21 15:31:25 +0100

    Added tempVars for parallel substitution

--- generator/src/main/kotlin/dobra101/mppcg/node/substitution/ParallelSubstitution.kt
+++ generator/src/main/kotlin/dobra101/mppcg/node/substitution/ParallelSubstitution.kt
@@ -1,5 +1,23 @@
 package dobra101.mppcg.node.substitution
 
+import dobra101.mppcg.node.expression.IdentifierExpression
+
 data class ParallelSubstitution(
     val substitutions: List<Substitution>
-) : Substitution("parallelSubstitution")
+) : Substitution("parallelSubstitution") {
+    val needTempVar: Set<IdentifierExpression>
+        get() {
+            val assignments = substitutions.filterIsInstance<AssignSubstitution>()
+
+            val identifierOnLhs = assignments.flatMap {
+                it.lhs.filterIsInstance<IdentifierExpression>()
+            }.toSet()
+
+            // TODO: stimmt nicht für z.b. x := y + 1
+            val identifierOnRhs = assignments.flatMap {
+                it.rhs.filterIsInstance<IdentifierExpression>()
+            }.toSet()
+
+            return identifierOnLhs.intersect(identifierOnRhs)
+        }
+}
